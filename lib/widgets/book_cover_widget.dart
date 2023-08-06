e87420fc369413fb0c40e7ae51370e290feb72d5
WeebNetsu <stephenvdw7777@gmail.com>
2023-08-06 12:20:30 +0200

    Added const to widget

--- lib/widgets/book_cover_widget.dart
+++ lib/widgets/book_cover_widget.dart
@@ -55,7 +55,7 @@ class _BookCoverWidgetState extends State<BookCoverWidget> {
           Navigator.of(modalSheetContext).pop();
           pageIndexNotifier.value = 0;
         },
-        child: Icon(Icons.close),
+        child: const Icon(Icons.close),
       ),
       child: BookInfoWidget(
         // handleDownloadBook: widget._handleDownloadBook,
